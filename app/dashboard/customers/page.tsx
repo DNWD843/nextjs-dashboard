40e7facbe8794dd8a6553246c6131fae630eaace
Dmitry Neklyudov <dima.kazanec.394@yandex.ru>
2024-03-27 00:25:41 +0300

    meta

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
 const CustomersPage = () => <p>Customers Page</p>;
 
 export default CustomersPage;
