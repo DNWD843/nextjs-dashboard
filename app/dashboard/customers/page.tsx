d386dafd7df1a29da1074afb366e36a3a42beeb8
Dmitry Neklyudov <dima.kazanec.394@yandex.ru>
2024-03-28 23:25:19 +0300

    customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,20 @@
 import { Metadata } from 'next';
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
 
+export const dynamic = 'force-dynamic';
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-const CustomersPage = () => <p>Customers Page</p>;
+const CustomersPage = async ({
+  searchParams,
+}: {
+  searchParams?: { query?: string; page?: string };
+}) => {
+  const customers = await fetchFilteredCustomers(searchParams?.query ?? '');
+
+  return <CustomersTable customers={customers} />;
+};
 
 export default CustomersPage;
